09f94191f10b09d92658a287c099aac5b7284fd2
rafael.estevamdesign@gmail.com <rafael.estevamdesign@gmail.com>
2023-05-22 23:10:27 -0300

    SETUP project

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
@@ -34,6 +36,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./public/_redirects", to: "./" },
+      ],
     })
   ]
 }
